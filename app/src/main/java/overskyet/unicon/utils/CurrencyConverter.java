ab20144ee53e4eef905e2f3a8eab27cbd61f7df6
Overskyet <paul.overskyet@gmail.com>
2022-04-16 16:23:14 +0100

    Improved conversion algorithm

--- app/src/main/java/overskyet/unicon/utils/CurrencyConverter.java
+++ app/src/main/java/overskyet/unicon/utils/CurrencyConverter.java
@@ -29,7 +29,7 @@ public final class CurrencyConverter {
         this.toCurrency = toCurrency;
         this.amount = amount;
         this.rates = rates;
-        return startConversion();
+        return isRateEntryEuro() ? startConversionInEuro() : startConversion();
     }
 
     public BigDecimal convert(String fromCurrency, String toCurrency, BigDecimal amount, int calculationAccuracy, Map<String, Double> rates) {
@@ -38,37 +38,51 @@ public final class CurrencyConverter {
         this.amount = amount;
         this.rates = rates;
         this.calculationAccuracy = calculationAccuracy < 0 ? this.calculationAccuracy : calculationAccuracy;
-        return startConversion();
+        return isRateEntryEuro() ? startConversionInEuro() : startConversion();
     }
 
     private BigDecimal startConversion() {
-        BigDecimal baseRate, targetRate, result;
+        BigDecimal baseRate, targetRate;
+        BigDecimal result = null;
 
-        if (rates == null || rates.isEmpty()) {
-            return null;
-        } else {
-            if (rates.containsKey(fromCurrency) && rates.containsKey(toCurrency)) {
-                baseRate = new BigDecimal(rates.get(fromCurrency).toString());
-                targetRate = new BigDecimal(rates.get(toCurrency).toString());
-                if (baseRate.compareTo(targetRate) < 0) {
-                    result = amount.multiply(targetRate).divide(baseRate, calculationAccuracy, RoundingMode.HALF_UP);
-                } else if (baseRate.compareTo(targetRate) > 0) {
-                    result = amount.divide(baseRate, calculationAccuracy, RoundingMode.HALF_UP).multiply(targetRate).setScale(calculationAccuracy, RoundingMode.HALF_UP);
-                } else {
-                    result = amount;
-                }
-            } else {
-                if (fromCurrency.equals("EUR") && !toCurrency.equals("EUR")) {
-                    targetRate = new BigDecimal(rates.get(toCurrency).toString());
-                    result = amount.multiply(targetRate).setScale(calculationAccuracy, RoundingMode.HALF_UP);
-                } else if (toCurrency.equals("EUR") && !fromCurrency.equals("EUR")) {
-                    baseRate = new BigDecimal(rates.get(fromCurrency).toString());
-                    result = amount.divide(baseRate, calculationAccuracy, RoundingMode.HALF_UP);
-                } else {
-                    result = amount;
-                }
+        if (isRateEntryExist()) {
+            baseRate = new BigDecimal(rates.get(fromCurrency).toString());
+            targetRate = new BigDecimal(rates.get(toCurrency).toString());
+
+            if (baseRate.compareTo(targetRate) < 0) {
+                result = amount.multiply(targetRate).divide(baseRate, calculationAccuracy, RoundingMode.HALF_UP);
+            } else if (baseRate.compareTo(targetRate) > 0) {
+                result = amount.divide(baseRate, calculationAccuracy, RoundingMode.HALF_UP).multiply(targetRate).setScale(calculationAccuracy, RoundingMode.HALF_UP);
             }
         }
         return result;
+    }
+
+    private BigDecimal startConversionInEuro() {
+        BigDecimal baseRate, targetRate;
+        BigDecimal result = null;
+
+        if (isOneOfRateEntriesExist()) {
+            if (fromCurrency.equals("EUR") && !toCurrency.equals("EUR")) {
+                targetRate = new BigDecimal(rates.get(toCurrency).toString());
+                result = amount.multiply(targetRate).setScale(calculationAccuracy, RoundingMode.HALF_UP);
+            } else if (toCurrency.equals("EUR") && !fromCurrency.equals("EUR")) {
+                baseRate = new BigDecimal(rates.get(fromCurrency).toString());
+                result = amount.divide(baseRate, calculationAccuracy, RoundingMode.HALF_UP);
+            }
+        }
+        return result;
+    }
+
+    private boolean isRateEntryExist() {
+        return (rates.containsKey(fromCurrency) && rates.containsKey(toCurrency));
+    }
+
+    private boolean isRateEntryEuro() {
+        return (fromCurrency.equals("EUR") || toCurrency.equals("EUR"));
+    }
+
+    private boolean isOneOfRateEntriesExist() {
+        return (rates.containsKey(fromCurrency) || rates.containsKey(toCurrency));
     }
 }
